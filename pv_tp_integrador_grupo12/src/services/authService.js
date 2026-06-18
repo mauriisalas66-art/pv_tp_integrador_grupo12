2fef6f1d7be83a30c97be2ca097697b46cc4c78d
nievesales <nievesaless45@gmail.com>
2026-06-18 15:26:23 -0300

    retorno de credenciales simuladas en servicio de login LuisNieves

--- pv_tp_integrador_grupo12/src/services/authService.js
+++ pv_tp_integrador_grupo12/src/services/authService.js
@@ -1,27 +1,22 @@
 // src/services/authService.js
 
-/**
-
- * @param {string} email 
- * @param {string} password 
- * @returns {Promise<Object>} 
- */
 export const loginService = async (email, password) => {
   if (!password || password.length < 8) {
     throw new Error("La contraseña debe tener al menos 8 caracteres.");
   }
-
   if (!email || !email.includes("@")) {
     throw new Error("Por favor, ingrese un correo electrónico válido.");
   }
 
   return new Promise((resolve) => {
     setTimeout(() => {
+      
       resolve({
+        nombre: "Admin temporal", 
         email: email,
         token: "fake-jwt-token-grupo12",
         status: "success"
       });
-    }, 1000);
+    }, 1500); 
   });
 };
