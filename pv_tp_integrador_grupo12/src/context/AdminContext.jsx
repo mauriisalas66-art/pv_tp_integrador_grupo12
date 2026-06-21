ad98946294d996ae21adb13749eb8ce54275db3f
nievesaless <nievesaless45@gmail.com>
2026-06-21 11:45:00 -0300

    definicion del estado inicial de autenticacion como null LuisNieves

--- pv_tp_integrador_grupo12/src/context/AdminContext.jsx
+++ pv_tp_integrador_grupo12/src/context/AdminContext.jsx
@@ -1,10 +1,9 @@
-// src/context/AdminContext.jsx
 import { createContext, useState } from 'react';
 
-export const AdminContext = createContext(null);
+export const AdminContext = createContext();
 
 export const AdminProvider = ({ children }) => {
-  const [admin, setAdmin] = useState(null); 
+  const [admin, setAdmin] = useState(null);
 
   return (
     <AdminContext.Provider value={{ admin, setAdmin }}>
