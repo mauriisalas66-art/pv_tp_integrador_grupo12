05b477d249e6d3cff3027366057bfbe6639ce4d6
nievesaless <nievesaless45@gmail.com>
2026-06-21 17:35:00 -0300

    LuisNieves conexion de boton ingresar con la accion global del contexto

--- pv_tp_integrador_grupo12/src/views/Login.jsx
+++ pv_tp_integrador_grupo12/src/views/Login.jsx
@@ -1,15 +1,30 @@
 // src/views/Login.jsx
-import { useState } from 'react';
+import { useState, useContext } from 'react'; 
+import { AdminContext } from '../context/AdminContext'; 
 
 export const Login = () => {
   const [nombre, setNombre] = useState('');
   const [email, setEmail] = useState('');
   const [sector, setSector] = useState('');
 
+  const { login } = useContext(AdminContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const datosOperador = {
+      nombre,
+      email,
+      sector
+    };
+
+    login(datosOperador);
+  };
+
   return (
     <div style={{ padding: '20px', maxWidth: '400px', margin: '0 auto' }}>
       <h2>Acceso al Sistema - Operador</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div style={{ marginBottom: '15px' }}>
           <label style={{ display: 'block', marginBottom: '5px' }}>Nombre del Operador:</label>
           <input 
