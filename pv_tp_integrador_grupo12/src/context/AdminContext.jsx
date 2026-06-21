ee819c628e0cd454d5d6798467b022ea6c377dee
nievesaless <nievesaless45@gmail.com>
2026-06-21 13:15:00 -0300

    LuisNieves: persistencia de datos de usuario con useEffect y localStorage

--- pv_tp_integrador_grupo12/src/context/AdminContext.jsx
+++ pv_tp_integrador_grupo12/src/context/AdminContext.jsx
@@ -1,19 +1,27 @@
 // src/context/AdminContext.jsx
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react'; 
 
 export const AdminContext = createContext();
 
 export const AdminProvider = ({ children }) => {
   const [admin, setAdmin] = useState(null);
+  useEffect(() => {
+    if (admin) {
+    
+      localStorage.setItem('operador_sesion', JSON.stringify(admin));
+    } else {
+    
+      localStorage.removeItem('operador_sesion');
+    }
+  }, [admin]);
 
   const login = (userData) => {
-    setAdmin(userData); 
+    setAdmin(userData);
   };
 
   const logout = () => {
     setAdmin(null);
   };
-
   return (
     <AdminContext.Provider value={{ admin, login, logout }}>
       {children}
